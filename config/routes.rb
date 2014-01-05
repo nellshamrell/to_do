8e99f3b8cd90f131cc6eecb9fcf711c6a9edd0a9
Nell Shamrell <nshamrell@bluebox.net>
2014-01-05 14:37:03 -0800

    updating stories, setting root page to projects list

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ ToDo::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'projects#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -43,7 +43,7 @@ ToDo::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
