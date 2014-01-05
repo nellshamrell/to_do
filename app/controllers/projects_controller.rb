93e2374155432de1a19690d59f27364390d21eec
Nell Shamrell <nshamrell@bluebox.net>
2014-01-04 19:57:11 -0800

    more controller refining and filling out specs

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -45,7 +45,6 @@ class ProjectsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_project
       @project = Project.find(params[:id])
     end
--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -68,4 +68,55 @@ describe ProjectsController do
     end
   end
 
+  describe "PUT update" do
+    context "with valid attributes" do
+      it "locates the requested project" do
+        put :update, id: project.id, project: attributes_for(:project)
+        assigns(:project).should == project
+      end
+
+      it "changes the project's attributes" do
+        put :update, id: project.id, project: attributes_for(:project, name: "something_else")
+        project.reload
+        project.name.should == "something_else"
+      end
+
+      it "redirects to the updated project" do
+        put :update, id: project.id, project: attributes_for(:project, name: "something_else")
+        response.should redirect_to project
+      end
+    end
+
+    context "with invalid attributes" do
+      it "locates the requested project" do
+        put :update, id: project.id, project: attributes_for(:project, name: nil )
+        assigns(:project).should == project
+      end
+
+      it "does not change the project's attributes" do
+        put :update, id: project.id, project: attributes_for(:project, name: nil )
+        project.reload
+        project.name.should_not be_nil
+      end
+
+      it "re-renders the edit template" do
+        put :update, id: project.id, project: attributes_for(:project, name: nil )
+        response.should render_template :edit
+      end
+    end
+  end
+
+  describe "DELETE destroy" do
+    it "deletes the project" do
+      expect{
+        delete :destroy, id: project
+      }.to change(Project,:count).by(-1)
+    end
+
+    it "redirects to projects#index" do
+      delete :destroy, id: project
+      response.should redirect_to projects_url
+    end
+  end
+
 end
