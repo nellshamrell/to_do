659f7e1b3994f4c80f1fe326147e4a208438bf11
Nell Shamrell <nshamrell@bluebox.net>
2014-01-05 13:55:40 -0800

    work in progress - adding specs and refining controller code

--- app/controllers/to_do_items_controller.rb
+++ app/controllers/to_do_items_controller.rb
@@ -1,31 +1,22 @@
 class ToDoItemsController < ApplicationController
   before_action :set_to_do_item, only: [:show, :edit, :update, :destroy]
 
-  # GET /to_do_items
-  # GET /to_do_items.json
   def index
     @to_do_items = ToDoItem.all
   end
 
-  # GET /to_do_items/1
-  # GET /to_do_items/1.json
   def show
   end
 
-  # GET /to_do_items/new
   def new
     @to_do_item = ToDoItem.new
   end
 
-  # GET /to_do_items/1/edit
   def edit
   end
 
-  # POST /to_do_items
-  # POST /to_do_items.json
   def create
     @to_do_item = ToDoItem.new(to_do_item_params)
-
     respond_to do |format|
       if @to_do_item.save
         format.html { redirect_to @to_do_item, notice: 'To do item was successfully created.' }
@@ -37,8 +28,6 @@ class ToDoItemsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /to_do_items/1
-  # PATCH/PUT /to_do_items/1.json
   def update
     respond_to do |format|
       if @to_do_item.update(to_do_item_params)
@@ -51,8 +40,6 @@ class ToDoItemsController < ApplicationController
     end
   end
 
-  # DELETE /to_do_items/1
-  # DELETE /to_do_items/1.json
   def destroy
     @to_do_item.destroy
     respond_to do |format|
@@ -62,7 +49,6 @@ class ToDoItemsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_to_do_item
       @to_do_item = ToDoItem.find(params[:id])
     end
--- spec/factories.rb
+++ spec/factories.rb
@@ -8,3 +8,14 @@ FactoryGirl.define do
     project
   end
 end
+
+class FactoryGirlHelper
+  # When using attributes_for(some_factory). Factory Girl ignores associations.  This allows us to include them when we need them.
+  # See http://stackoverflow.com/a/10294322 for more info
+
+  def self.build_attributes(*args)
+    FactoryGirl.build(*args).attributes.delete_if do |k, v|
+        ["id", "created_at", "updated_at"].member?(k)
+    end
+  end
+end
